8ad865544f773e12ef73d7ba19c38c33724bc38c
Raquel <raquelsofia96@gmail.com>
2020-12-01 23:02:53 +0000

    Day 1 Completed

---
+++ 1.py
@@ -0,0 +1,45 @@
+#find the two entries that sum to 2020 and then multiply those two numbers together
+
+def loadData():
+    data = []
+    with open("1input.txt") as f:
+        for line in f.readlines():
+            data.append(line)
+    return data
+
+def sum2To2020(data):
+    for i in range(len(data)-1):
+        for n in range(i+1, len(data)):
+            if int(data[i]) + int(data[n]) == 2020:
+                print(data[i], data[n])
+                return (int(data[i]), int(data[n]))
+
+def sum3To2020(data):
+    for i in range(len(data)-1):
+        for n in range(i+1, len(data)):
+            for m in range(n+1, len(data)):
+                if int(data[i]) + int(data[n]) + int(data[m]) == 2020:
+                    print(data[i], data[n], data[m])
+                    return (int(data[i]), int(data[n]), int(data[m]))
+
+def multiply(entries):
+    result =  entries[0] * entries[1]
+    return result
+
+def productOf2():
+    data = loadData()
+    entries = sum2To2020(data)
+    result = multiply(entries)
+    print(result)
+
+def productOf3():
+    data = loadData()
+    entries = sum3To2020(data)
+    firstMultiply = multiply(entries)
+    lastEntry = (firstMultiply, entries[2])
+    result = multiply(lastEntry)
+    print(result)
+
+
+productOf2()
+productOf3()
